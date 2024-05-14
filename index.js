63bbc6c5bdbd2560be612293d1cf8ff9b839055a
Nate Waldron <nate@landistechnologies.com>
2024-05-14 12:11:05 -0400

    update-checker

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import { updateChecker } from './update-checker.js';
+
+updateChecker();
+
 const welcomeText = document.createElement('h3');
 welcomeText.textContent = "Hello from index.js!";
 document.body.appendChild(welcomeText);
