29e78ae933731d196537800bec1b0be4071408a8
Nate Waldron <nate@landistechnologies.com>
2024-05-14 12:14:20 -0400

    test didnt work, deleting

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-import { updateChecker } from './update-checker.js';
-
-updateChecker();
-
 const welcomeText = document.createElement('h3');
 welcomeText.textContent = "Hello from index.js!";
 document.body.appendChild(welcomeText);
-
-const p = document.createElement('p');
-p.textContent = "testing";
-document.body.appendChild(p);
