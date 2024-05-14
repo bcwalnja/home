e17aa9fbc85c7282973a45135b80fd3a0714014c
Nate Waldron <nate@landistechnologies.com>
2024-05-14 12:13:08 -0400

    test

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ updateChecker();
 const welcomeText = document.createElement('h3');
 welcomeText.textContent = "Hello from index.js!";
 document.body.appendChild(welcomeText);
+
+const p = document.createElement('p');
+p.textContent = "testing";
+document.body.appendChild(p);
