922b8f81900f956a33b4901e844be55c90641882
Nate <bcwalnja@gmail.com>
2024-07-11 21:48:04 -0400

    added start button

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var logVerbose = false;
-var canvas = document.getElementById('container');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-canvas.style.backgroundColor = 'black';
-var qVelocity,
+var canvas,
+  qVelocity,
   context,
   font,
   fontSize,
@@ -25,56 +22,108 @@ var explosions = [];
 // attach a listener to the window resize event
 // to keep the canvas sized to the window
 window.addEventListener('resize', function () {
+  initCanvas();
+});
+
+showMainMenu();
+
+function showMainMenu() {
+  //clear the canvas
+  initCanvas();
+  initContext();
+  initVariables();
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  var width = context.measureText("Start").width;
+  context.fillText("Start", canvas.width / 2, canvas.height / 2);
+  // start button
+  var startButton = {
+    text: "Start",
+    x: canvas.width / 2 - width / 2,
+    y: canvas.height / 2 - fontSize / 2,
+    onClicked: startGame
+  };
+  clickableTextObjects.push(startButton);
+
+  // box around start button
+  var left = startButton.x + padding / 2;
+  var top = startButton.y - fontSize + padding;
+  var width = width + padding * 4;
+  var height = fontSize + padding;
+  context.strokeRect(left, top, width, height);
+}
+
+function startGame() {
+  if (!canvas) {
+    initCanvas()
+  }
+  if (!context) {
+    initContext();
+  }
+  initVariables();
+  initGame();
+  initTimer();
+  animate();
+}
+
+function initCanvas() {
+  canvas = document.getElementById('container');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   boundaryX = canvas.width;
   boundaryY = canvas.height;
+  canvas.addEventListener('click', handleCanvasClick());
+}
+
+function initContext() {
+  context = canvas.getContext('2d');
+  fontSize = Math.floor(canvas.height / 20);
+  font = fontSize + 'px Arial';
   context.font = font;
   context.fillStyle = 'white';
   context.lineWidth = 2;
   context.strokeStyle = 'white';
-});
+}
 
-canvas.addEventListener('click', function (event) {
-  var x = event.clientX;
-  var y = event.clientY;
-  var objectWasClicked = false;
-  clickableTextObjects.forEach(function (obj) {
-    if (areOverlapping({ x: x, y: y }, obj)) {
-      objectWasClicked = true;
-      if (obj.onClicked) {
-        obj.onClicked(obj);
-      }
-    }
-  });
-  if (!objectWasClicked) {
-    addExplosion(x, y);
-  }
-});
-
-init();
-animate();
-
-function init() {
-  qVelocity = canvas.height / 1000;
-  context = canvas.getContext('2d');
-  context.lineWidth = 2;
-  context.strokeStyle = 'white';
+function initVariables() {
+  //init variables
   explosionDuration = 5000;
+  qVelocity = canvas.height / 1000;
+  
+  padding = canvas.height * .02;
+  a = [];
+}
 
+function handleCanvasClick() {
+  return function (event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var objectWasClicked = false;
+    clickableTextObjects.forEach(function (obj) {
+      if (areOverlapping({ x: x, y: y }, obj)) {
+        objectWasClicked = true;
+        if (obj.onClicked) {
+          obj.onClicked(obj);
+        }
+      }
+    });
+    if (!objectWasClicked) {
+      addExplosion(x, y);
+    }
+  };
+}
+
+function initGame() {
   // TODO: make these configurable
   term1Min = 1;
   term1Max = 12;
   term2Min = 1;
   term2Max = 12;
+  
+  generateNewQuestion();
+  generateNewAnswers();
+}
 
-  fontSize = Math.floor(canvas.height / 20);
-  font = fontSize + 'px Arial';
-  context.font = font;
-  context.fillStyle = 'white';
-  padding = canvas.height * .02;
-  a = [];
-
+function initTimer() {
   startTime = Date.now();
   timer = {};
   timer.x = 10;
@@ -82,9 +131,6 @@ function init() {
   timer.text = '120';
   timer.onClicked = timerOnClicked;
   clickableTextObjects.push(timer);
-
-  generateNewQuestion();
-  generateNewAnswers();
 }
 
 function draw() {
